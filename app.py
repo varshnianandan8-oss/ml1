20fb894f7e7cc74af807176edeb259fefd23fcf7
varshnianandan8-oss <varshnianandan8@gmail.com>
2026-03-26 19:30:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,24 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
-
+import os
 
 app = Flask(__name__)
 
-model = pickle.load(open("model.pkl","rb"))
-
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+model_path = os.path.join(BASE_DIR, "model.pkl")
+model = pickle.load(open(model_path, "rb"))
 
 @app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route("/predict",methods=["POST"])
+@app.route("/predict", methods=["POST"])
 def predict():
     income = float(request.form["income"])
     score = float(request.form["score"])
 
-    data = np.array([[income,score]])
+    data = np.array([[income, score]])
     result = model.predict(data)[0]
- 
-    return render_template("result.html",prediction=result)
 
-   
+    return render_template("result.html", prediction=result)
